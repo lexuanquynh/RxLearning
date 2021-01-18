de0d79ed7d1336128b12268750e81c7499b9901d
Le Xuan Quynh <le-xuan-quynh@goodcycle.net>
2021-01-18 10:36:23 +0900

    add contents

--- RxLearning/Playground/SubjectPlayground.playground/Contents.swift
+++ RxLearning/Playground/SubjectPlayground.playground/Contents.swift
@@ -7,5 +7,11 @@ public func example(of description: String, action: () -> Void) {
 }
 
 example(of: "PublishSubject") {
-  let subject = PublishSubject<String>()
+    let subject = PublishSubject<String>()
+    subject.onNext("Co ai nghe tui noi khong?")
+    let subscriptionOne = subject
+        .subscribe(onNext: { string in
+            print(string)
+        })
+    subject.on(.next("1"))
 }
