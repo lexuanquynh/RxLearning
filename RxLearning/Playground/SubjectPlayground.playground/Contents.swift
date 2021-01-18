f719f7f0b36cb23f4528e99aadcefb2f398b8cdc
Le Xuan Quynh <le-xuan-quynh@goodcycle.net>
2021-01-18 10:00:59 +0900

    add subject playground

---
+++ RxLearning/Playground/SubjectPlayground.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+import RxSwift
+
+public func example(of description: String, action: () -> Void) {
+    print("\n--- Example of:", description, "---")
+    action()
+}
+
+example(of: "PublishSubject") {
+  let subject = PublishSubject<String>()
+}
