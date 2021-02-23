2a02fade91f4724aa42bcd179ed1addb311a8529
Le Xuan Quynh <le-xuan-quynh@goodcycle.net>
2021-02-23 21:52:06 +0900

    update

--- RxLearning/Playground/SubjectPlayground.playground/Contents.swift
+++ RxLearning/Playground/SubjectPlayground.playground/Contents.swift
@@ -11,7 +11,31 @@ example(of: "PublishSubject") {
     subject.onNext("Co ai nghe tui noi khong?")
     let subscriptionOne = subject
         .subscribe(onNext: { string in
-            print(string)
+            print("1)" + string)
         })
     subject.on(.next("1"))
+
+    // Add subscription two
+    let subscriptionTwo = subject
+      .subscribe { event in
+        print("2)", event.element ?? event)
+      }
+
+    subscriptionOne.dispose()
+    subject.onNext("4")
+
+    // 1
+    subject.onCompleted()
+    // 2
+    subject.onNext("5")
+    // 3
+    subscriptionTwo.dispose()
+    let disposeBag = DisposeBag()
+    // 4
+    subject
+      .subscribe {
+        print("3)", $0.element ?? $0)
+      }.disposed(by: disposeBag)
+
+    subject.onNext("?")
 }
